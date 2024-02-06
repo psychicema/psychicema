d79e8c4aa9ba72a7c7c803aeac1b39e48aea5232
psychicema <159125180+psychicema@users.noreply.github.com>
2024-02-06 01:49:42 -0800

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://thegreatmoses.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://thegreatmoses.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://thegreatmoses.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://thegreatmoses.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://thegreatmoses.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://thegreatmoses.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
